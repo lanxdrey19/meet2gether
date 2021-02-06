3058ff9838c341c728428bca25655cd67d1491ac
Lance Delos Reyes <lan_drey19@hotmail.com>
2021-02-06 21:54:32 +1300

    added calendar and toolbar component

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons';
+import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
+import FullCalendar, { formatDate } from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+//import { INITIAL_EVENTS, createEventId } from './event-utils'
 
 function App() {
+
+  const handleDateClick = (arg : any) => { // bind with an arrow function
+    alert(arg.dateStr)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+  <Toolbar>
+    <IconButton edge="start" color="inherit" aria-label="menu">
+      <MenuOutlinedIcon />
+    </IconButton>
+    <Typography variant="h6" >
+      Meet2Gether
+    </Typography>
+    <Button color="inherit">My Calendar</Button>
+    <Button color="inherit">Team Calendar</Button>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
+
+      <FullCalendar
+        plugins={[ dayGridPlugin,timeGridPlugin,interactionPlugin ]}
+        dateClick={handleDateClick}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }}
+        
+      />
     </div>
   );
+
+  
 }
 
 export default App;
